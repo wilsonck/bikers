535c760f022451958a06fe309434f721daa2ba59
wilson.ck@gmail.com <leticia1808>
2015-04-02 02:17:40 -0300

    Projeto finalizado - hora de ir domir!

--- js/app.js
+++ js/app.js
@@ -13,6 +13,7 @@ app.controller('bikersCtrl', ['$scope', '$http', '$q', '$sce', function ($scope,
 
                 this.fnEvents();
                 this.fnEventsScope();
+                this.fnLoadJsonBikers()
 
                 /*** init object days of week */
                 $scope.daysWeek = {
@@ -83,17 +84,19 @@ app.controller('bikersCtrl', ['$scope', '$http', '$q', '$sce', function ($scope,
                         alert('Please choose Ride in Group !');
                         return;
                     }
-                    var daysWeekChoose = daysChoose();
+                    var daysWeekChoose = appBikers.fnEventsData.daysChoose();
                     if(daysWeekChoose == ''){
                         alert('Please choose a days of the week !');
                         return;
                     }
-                    var dataToday = getDate();
-                    var hourNow = getHour();
+                    var _city = (($scope.input.city === '') || ($scope.input.city === undefined)) ? '.' : $scope.input.city;
+                    console.log(city)
+                    var dataToday = appBikers.fnEventsData.getDate();
+                    var hourNow = appBikers.fnEventsData.getHour();
                     $scope.bikers.push({
                         name: $scope.input.name,
                         email: $scope.input.email,
-                        city: $scope.input.city,
+                        city: _city,
                         rideGroup: $scope.input.rideGroup,
                         daysOfWeek: daysWeekChoose,
                         registration:[{
@@ -105,121 +108,114 @@ app.controller('bikersCtrl', ['$scope', '$http', '$q', '$sce', function ($scope,
                     $scope.reseteFields();
                 }
 
-            }
+            },
+            fnEventsData:{
 
+                /*********
+                /* Pega as variaves de escopo dos dias selecionados e compara
+                * se é fim de semana
+                * se é meio de semana
+                * se ele faz todos os dias
+                */
+                daysChoose: function(){
+                    var dayChoose = [];
+                    for (var prop in $scope.daysWeek) {
+                        if( $scope.daysWeek[prop] ) dayChoose.push(prop)
+                    }
+                    var daysWeek = ["Mon", "Tue", "Wed", "Thu", "Fri"];
+                    var weekends = ["Sat", "Sun"];
+                    var amountLengthArry = dayChoose.length;
+                    //Caso tenha 2 selecionado pode ser que ele faça aos fim de semana
+                    if (amountLengthArry === 2){
+                        for (var i = 0 ; i < amountLengthArry; i++) {
+                            var dayCompare = dayChoose[i];
+                            for (var j=0, totalArrayWeek = daysWeek.length; j < totalArrayWeek; j++){
+                                var dayWeek = daysWeek[j];
+                                if (dayWeek === dayCompare) { 
+                                    return dayChoose;
+                                }
+                            };
+                        };
+                        return 'Weekends';
+                    // Se ele marcou 5 pode ser que faça no meio de semana    
+                    }else if(amountLengthArry === 5){
+                        for (var i = 0 ; i < amountLengthArry; i++) {
+                            var dayCompare = dayChoose[i];
+                            for (var j=0, totalArrayWeek = weekends.length; j < totalArrayWeek; j++){
+                                var dayWeek = weekends[j];
+                                if (dayWeek === dayCompare) { 
+                                    return dayChoose;
+                                }
+                            };
+                        };
+                        return 'Week Days';
+                    // Se marcou 7 ele faz todos os dias
+                    }else if(amountLengthArry === 7){
+                        return 'Every day';
+                    // Senão retorna os dias
+                    }else{
+                        return dayChoose;
+                    }
+
+                },
+                getDate:function(){
+                    var today = new Date();
+                    var dd = today.getDate();
+                    var mm = today.getMonth()+1; //January is 0!
+
+                    var yyyy = today.getFullYear();
+                    if(dd<10){
+                        dd='0'+dd
+                    } 
+                    if(mm<10){
+                        mm='0'+mm
+                    } 
+                    var today = dd+'/'+mm+'/'+yyyy;
+                    return today;
+                },
+                getHour:function(){
+                    var today = new Date();
+                    var h = today.getHours();
+                    var m = today.getMinutes();
+                    if(m<10){
+                        m='0'+m
+                    } 
+                    var AMPM = (h > 12) ? 'PM' : 'AM';
+                    var s = today.getSeconds();
+                    var hourComplete = h+":"+m+AMPM;
+                    return hourComplete
+                }
+            },
+            fnLoadJsonBikers:function(){
+
+                var promise = BikersOpenData();
+
+                promise.then(function(_result) {
+                    $scope.bikers = _result.bikers;
+                }, function(reason) {
+                    alert('Failed: ' + _result);
+                });
+
+                function BikersOpenData(){
+                    var deferred = $q.defer();
+                    $http.get('json/bikers.json').then(function (_result) {
+                        if (typeof _result.data === 'object') {
+                            deferred.resolve(_result.data);
+                        } else {
+                            console.log('erro -->',_result);
+                            $q.reject(_result.data);
+                        }
+                   }, function (_result) {
+                        console.log('erro -->',_result);
+                        deferred.reject(_result);
+                    });
+                    return deferred.promise;
+                }
+
+            }
         }
     }());
 
     appBikers.init();
-
-
-
-
-
-    
-
-
-
-    /*********
-    /* Pega as variaves de escopo dos dias selecionados e compara
-    * se é fim de semana
-    * se é meio de semana
-    * se ele faz todos os dias
-    */
-    function daysChoose(){
-        var dayChoose = [];
-        for (var prop in $scope.daysWeek) {
-            if( $scope.daysWeek[prop] ) dayChoose.push(prop)
-        }
-        var daysWeek = ["Mon", "Tue", "Wed", "Thu", "Fri"];
-        var weekends = ["Sat", "Sun"];
-        var amountLengthArry = dayChoose.length;
-        //Caso tenha 2 selecionado pode ser que ele faça aos fim de semana
-        if (amountLengthArry === 2){
-            for (var i = 0 ; i < amountLengthArry; i++) {
-                var dayCompare = dayChoose[i];
-                for (var j=0, totalArrayWeek = daysWeek.length; j < totalArrayWeek; j++){
-                    var dayWeek = daysWeek[j];
-                    if (dayWeek === dayCompare) { 
-                        return dayChoose;
-                    }
-                };
-            };
-            return 'Weekends';
-        // Se ele marcou 5 pode ser que faça no meio de semana    
-        }else if(amountLengthArry === 5){
-            for (var i = 0 ; i < amountLengthArry; i++) {
-                var dayCompare = dayChoose[i];
-                for (var j=0, totalArrayWeek = weekends.length; j < totalArrayWeek; j++){
-                    var dayWeek = weekends[j];
-                    if (dayWeek === dayCompare) { 
-                        return dayChoose;
-                    }
-                };
-            };
-            return 'Week Days';
-        // Se marcou 7 ele faz todos os dias
-        }else if(amountLengthArry === 7){
-            return 'Every day';
-        // Senão retorna os dias
-        }else{
-            return dayChoose;
-        }
-
-    }
-
-    function getDate(){
-        var today = new Date();
-        var dd = today.getDate();
-        var mm = today.getMonth()+1; //January is 0!
-
-        var yyyy = today.getFullYear();
-        if(dd<10){
-            dd='0'+dd
-        } 
-        if(mm<10){
-            mm='0'+mm
-        } 
-        var today = dd+'/'+mm+'/'+yyyy;
-        return today;
-    }
-
-    function getHour(){
-        var today = new Date();
-        var h = today.getHours();
-        var m = today.getMinutes();
-        if(m<10){
-            m='0'+m
-        } 
-        var AMPM = (h > 12) ? 'PM' : 'AM';
-        var s = today.getSeconds();
-        var hourComplete = h+":"+m+AMPM;
-        return hourComplete
-    }
-
-    var promise = BikersOpenData();
-
-    promise.then(function(_result) {
-        $scope.bikers = _result.bikers;
-    }, function(reason) {
-        alert('Failed: ' + _result);
-    });
-
-
-    function BikersOpenData(){
-        var deferred = $q.defer();
-        $http.get('json/bikers.json').then(function (_result) {
-            if (typeof _result.data === 'object') {
-                deferred.resolve(_result.data);
-            } else {
-                console.log('erro -->',_result);
-                $q.reject(_result.data);
-            }
-       }, function (_result) {
-            console.log('erro -->',_result);
-            deferred.reject(_result);
-        });
-        return deferred.promise;
-    }
 
 }])
